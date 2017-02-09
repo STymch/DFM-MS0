415002aab3eae0001ecc61a0c918d241aaf6dd15
Sergiy Tymchenko <stymch@outlook.com>
2017-02-09 18:34:18 +0200

    09.02.2017

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main() {
+	std::vector<int> vect = { 1, 2, 3 };
+	std::transform(vect.begin(), vect.end(), vect.begin(),
+		[](int value) { return value + 1; });
+	for (int value : vect) {
+		std::cout << value << std::endl;
+	}
+	
+	getchar();
+	
+	return 0;
+}
